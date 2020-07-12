aa5c5653fbad54733cde85c76b0fbee0c7b25036
somurie06 <somurie06@gmail.com>
2020-07-12 19:07:27 +0900

    スマホ画面向けStoreアイコンが完成

--- components/store/StoreIcon.vue
+++ components/store/StoreIcon.vue
@@ -1,40 +1,28 @@
 <template>
-
-              <v-layout row justify-center>
-                <button onclick="alert('AppStore申請中')" class="storeButton">
-                  <v-layout row justify-center>
-                    <v-flex>
-                      <img :src="require('@/assets/store/apple_store_logo.svg')" width="28"/>
-                    </v-flex>
-                    <v-flex class="textArea">
-                      <h5>Download on</h5>
-                      <h4>App Store</h4>
-                    </v-flex>
-                  </v-layout>
-                </button>
-                <button onclick="alert('GooglePlay申請中')" class="storeButton">
-                  <v-layout row justify-center>
-                    <v-flex>
-                      <img :src="require('@/assets/store/google_play_logo.svg')" width="28"/>
-                    </v-flex>
-                    <v-flex class="textArea">
-                      <h5>Download on</h5>
-                      <h4>GooglePlay</h4>
-                    </v-flex>
-                  </v-layout>
-                </button>
-<!--                <button class="storeButton" onclick="alert('AppStore申請中')">-->
-<!--                  <div style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2016-06-01&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"-->
-<!--                  ></div>-->
-<!--                </button>-->
-
-<!--                <button class="storeButton" onclick="alert('GooglePlay申請中')">-->
-<!--                  <img alt="Google Play で手に入れよう" src="https://play.google.com/intl/en_us/badges/images/generic/ja_badge_web_generic.png"-->
-<!--                       width="155"-->
-<!--                       height="60"-->
-<!--                  />-->
-<!--                </button>-->
-              </v-layout>
+  <v-layout row justify-center>
+    <button onclick="alert('AppStore申請中')" class="storeButton">
+      <v-layout row justify-center>
+        <v-flex>
+          <img :src="require('@/assets/store/apple_store_logo.svg')" width="28"/>
+        </v-flex>
+        <v-flex class="textArea">
+          <h5>Download on</h5>
+          <h4>App Store</h4>
+        </v-flex>
+      </v-layout>
+    </button>
+    <button onclick="alert('GooglePlay申請中')" class="storeButton">
+      <v-layout row justify-center>
+        <v-flex>
+          <img :src="require('@/assets/store/google_play_logo.svg')" width="28"/>
+        </v-flex>
+        <v-flex class="textArea">
+          <h5>Download on</h5>
+          <h4>GooglePlay</h4>
+        </v-flex>
+      </v-layout>
+    </button>
+  </v-layout>
 </template>
 
 <script>
@@ -53,7 +41,8 @@ export default {
   }
   .storeButton {
     z-index: 3;
-    background-color: #05FBFF;
+    background-color: #4FD360; /* メインの緑より一段回明るい色 */
+    /*background-color: #3FC350;*/
     padding: 10px 30px 10px 30px;
     margin-left: 10px;
     margin-right: 10px;
@@ -76,6 +65,8 @@ export default {
     line-height: 1.2;
   }
   @media (max-width: 600px) {
-
+    .storeButton {
+      width: 160px;
+    }
   }
 </style>
